ccb9fdc03d7707f9d06b4b4970b6df3b72ac2e8e
Marek Siarkowicz <mareksiarkowicz@gmail.com>
2014-06-04 16:42:32 +0200

    Refactor.

--- src/algorithm.cpp
+++ src/algorithm.cpp
@@ -10,12 +10,12 @@
 typedef std::shared_ptr<Tournament> tournament_ptr;
 
 
-bool check_if_fixable(TournamentGraph& graph, int node)
+bool check_if_fixable(TournamentGraph& graph, int winner)
 {
     int wins = 0;
     for (int i=0; i<graph.get_size(); ++i)
     {
-        if(i != node && graph.wins(node, i))
+        if(i != winner && graph.wins(winner, i))
             wins++;
     }
     if (wins < log2(graph.get_size()))
@@ -24,7 +24,7 @@ bool check_if_fixable(TournamentGraph& graph, int node)
     {
         for (int i=0; i<graph.get_size(); ++i)
         {
-            if(i == node)
+            if(i == winner)
                 continue;
             wins = 0;
             for (int j=0; j<graph.get_size(); ++j)
@@ -44,7 +44,7 @@ bool check_if_fixable(TournamentGraph& graph, int node)
 }
 
 
-bool check_if_case_A(TournamentGraph& graph, int node)
+bool check_if_case_A(TournamentGraph& graph, int winner)
 {
     int wins = 0;
     int max_wins_from_losses = 0;
@@ -52,7 +52,7 @@ bool check_if_case_A(TournamentGraph& graph, int node)
             
     for (int i=0; i<graph.get_size(); ++i)
     {
-        if(i != node && graph.wins(node, i))
+        if(i != winner && graph.wins(winner, i))
             wins++;
         else
         {
@@ -74,14 +74,14 @@ bool check_if_case_A(TournamentGraph& graph, int node)
 }
 
 
-tournament_ptr fix_tournament_A(TournamentGraph& graph, int node)
+tournament_ptr fix_tournament_A(TournamentGraph& graph, int winner)
 {
     std::set<tournament_ptr> won_tournaments, lost_tournaments;
     for (int i=0; i<graph.get_size(); ++i)
     {
-        if(node == i)
+        if(winner == i)
             continue;
-        if(graph.wins(node, i))
+        if(graph.wins(winner, i))
             won_tournaments.insert(tournament_ptr(new Tournament(&graph, i)));
         else
             lost_tournaments.insert(tournament_ptr(new Tournament(&graph, i)));    
@@ -115,7 +115,7 @@ tournament_ptr fix_tournament_A(TournamentGraph& graph, int node)
             if (++i==won_tournaments.end())
             {
                 new_won_tournaments.insert(tournament_ptr(
-                    new Tournament(*compatitor1, Tournament(&graph, node))));
+                    new Tournament(*compatitor1, Tournament(&graph, winner))));
                 break;
             }
             auto compatitor2 = *i;
@@ -130,27 +130,27 @@ tournament_ptr fix_tournament_A(TournamentGraph& graph, int node)
  }
 
 
-bool check_if_case_B(TournamentGraph& graph, int node)
+bool check_if_case_B(TournamentGraph& graph, int winner)
 {
     int wins = 0;
     for (int i=0; i<graph.get_size(); ++i)
     {
-        if(i != node && graph.wins(node, i))
+        if(i != winner && graph.wins(winner, i))
             wins++;
     }
     return wins >= graph.get_size() / 2;
 }
 
 
-tournament_ptr fix_tournament_B(TournamentGraph &graph, int node)
+tournament_ptr fix_tournament_B(TournamentGraph &graph, int winner)
 {
     std::set<tournament_ptr> won_tournaments, lost_tournaments;
     std::set<tournament_ptr> new_won_tournaments, new_lost_tournaments;
     for (int i=0; i<graph.get_size(); ++i)
     {
-        if(node == i)
+        if(winner == i)
             continue;
-        if(graph.wins(node, i))
+        if(graph.wins(winner, i))
             won_tournaments.insert(tournament_ptr(new Tournament(&graph, i)));
         else
             lost_tournaments.insert(tournament_ptr(new Tournament(&graph, i)));    
@@ -196,7 +196,7 @@ tournament_ptr fix_tournament_B(TournamentGraph &graph, int node)
             if (++i==won_tournaments.end())
             {
                 new_won_tournaments.insert(tournament_ptr(
-                    new Tournament(*compatitor1, Tournament(&graph, node))));
+                    new Tournament(*compatitor1, Tournament(&graph, winner))));
                 break;
             }
             auto compatitor2 = *i;
@@ -227,15 +227,15 @@ tournament_ptr fix_tournament_B(TournamentGraph &graph, int node)
 }
 
 
-bool check_if_case_C(TournamentGraph& graph, int node)
+bool check_if_case_C(TournamentGraph& graph, int winner)
 {
     std::set<int> won_with, lost_with;
     int log2n = std::log2(graph.get_size());
     for (int i=0;i<graph.get_size(); ++i)
     {
-        if(i == node)
+        if(i == winner)
             continue;
-        if(graph.wins(node, i))
+        if(graph.wins(winner, i))
             won_with.insert(i);
         else
             lost_with.insert(i);
@@ -257,16 +257,16 @@ bool check_if_case_C(TournamentGraph& graph, int node)
 }
 
 
-tournament_ptr fix_tournament_C(TournamentGraph& graph, int node)
+tournament_ptr fix_tournament_C(TournamentGraph& graph, int winner)
 {
     std::set<tournament_ptr> won_tournaments, lost_tournaments;
     std::set<tournament_ptr> new_won_tournaments, new_lost_tournaments;
-    tournament_ptr node_tournament(new Tournament(&graph, node));
+    tournament_ptr node_tournament(new Tournament(&graph, winner));
     for (int i=0; i<graph.get_size(); ++i)
     {
-        if(node == i)
+        if(winner == i)
             continue;
-        if(graph.wins(node, i))
+        if(graph.wins(winner, i))
             won_tournaments.insert(tournament_ptr(new Tournament(&graph, i)));
         else
             lost_tournaments.insert(tournament_ptr(new Tournament(&graph, i)));    
@@ -317,7 +317,7 @@ tournament_ptr fix_tournament_C(TournamentGraph& graph, int node)
             if (++i==won_tournaments.end())
             {
                 new_won_tournaments.insert(tournament_ptr(
-                    new Tournament(*compatitor1, Tournament(&graph, node))));
+                    new Tournament(*compatitor1, Tournament(&graph, winner))));
                 break;
             }
             auto compatitor2 = *i;
@@ -333,26 +333,26 @@ tournament_ptr fix_tournament_C(TournamentGraph& graph, int node)
 }
 
 
-void fix_tournament(TournamentGraph& graph, int node)
+void fix_tournament(TournamentGraph& graph, int winner)
 {
-    if(check_if_fixable(graph, node))
+    if(check_if_fixable(graph, winner))
     {
         std::shared_ptr<Tournament> tournament;
         printf("Fixable\n");
-        if (check_if_case_A(graph, node))
+        if (check_if_case_A(graph, winner))
         {
             printf("Case A\n");    
-            tournament = fix_tournament_A(graph, node);
+            tournament = fix_tournament_A(graph, winner);
         }
-        else if (check_if_case_B(graph, node))
+        else if (check_if_case_B(graph, winner))
         {
             printf("Case B\n");
-            tournament = fix_tournament_B(graph, node);
+            tournament = fix_tournament_B(graph, winner);
         }
-        else if (check_if_case_C(graph, node))
+        else if (check_if_case_C(graph, winner))
         {
             printf("Case C\n");
-            tournament = fix_tournament_C(graph, node);
+            tournament = fix_tournament_C(graph, winner);
         }
         else
         {
